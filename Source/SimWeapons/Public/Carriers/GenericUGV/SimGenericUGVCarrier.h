472a624f22c99e63aa3c405b1634af2713eb3ccb
DanyloBeha <d.beha@ukma.edu.ua>
2026-06-10 23:05:39 +0300

    Add generic ugv carrier

--- Source/SimWeapons/Public/Carriers/GenericUGV/SimGenericUGVCarrier.h
+++ Source/SimWeapons/Public/Carriers/GenericUGV/SimGenericUGVCarrier.h
@@ -2,5 +2,50 @@
 
 #pragma once
 
-// Placeholder for generic UGV weapon carrier.
-// This class/file will be used for ground vehicle weapon carrier logic in the future.
+#include "CoreMinimal.h"
+#include "Carriers/SimWeaponCarrierComponent.h"
+#include "SimGenericUGVCarrier.generated.h"
+
+UCLASS(Blueprintable, BlueprintType, ClassGroup = (SimWeapons), meta = (BlueprintSpawnableComponent))
+class SIMWEAPONS_API USimGenericUGVCarrier : public USimWeaponCarrierComponent
+{
+	GENERATED_BODY()
+
+public:
+	USimGenericUGVCarrier();
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType,
+		FActorComponentTickFunction* ThisTickFunction) override;
+
+	virtual void ApplyCarrierDamage(float DamageAmount) override;
+
+	UFUNCTION(BlueprintCallable, Category = "Sim Weapons|Carrier")
+	bool CanFireWhileMoving() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Sim Weapons|Carrier")
+	float GetArmorRating() const;
+
+	UFUNCTION(BlueprintCallable, Category = "Sim Weapons|Carrier")
+	float GetEnergyRegenRate() const;
+
+protected:
+	virtual void BeginPlay() override;
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Sim Weapons|Carrier")
+	void OnCarrierDestroyed();
+
+	UFUNCTION(BlueprintImplementableEvent, Category = "Sim Weapons|Carrier")
+	void OnEnergyDepleted();
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sim Weapons|Carrier", meta = (ClampMin = "0.0"))
+	float ArmorRating = 20.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sim Weapons|Carrier", meta = (ClampMin = "0.0"))
+	float EnergyRegenRate = 10.0f;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Sim Weapons|Carrier")
+	bool bCanFireWhileMoving = true;
+
+private:
+	bool bEnergyDepletedEventFired = false;
+};
